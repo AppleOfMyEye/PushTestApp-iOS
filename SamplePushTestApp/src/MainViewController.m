8030f7c7ee68c6e9c3047a7f9822c53707c95ef1
Nicolas Miyasato <nmiyasato@gmail.com>
2014-07-01 10:35:20 -0300

    Agregando Alerts.

--- SamplePushTestApp/src/MainViewController.m
+++ SamplePushTestApp/src/MainViewController.m
@@ -39,51 +39,69 @@
 
 
 - (IBAction)subscribe:(id)sender {
+    __weak MainViewController *safeMe = self;
+
     [self.application.pushNotifications subscribeDeviceWithToken:[self.deviceToken description]
                                                        toChannel:self.subscribeTextField.text
                                                       completion:^(KZResponse *r) {
                                                           if (r.error == nil) {
-                                                              NSLog(@"response is %@", r.response);
-                                                              NSLog(@"Did Subscribe");
+                                                              NSString *message = [NSString stringWithFormat:@"Subscribed to %@", self.subscribeTextField.text];
+                                                              [safeMe showAlertWith:message title:@"Subscribed!"];
+                                                          } else {
+                                                              [safeMe showAlertWith:[r.error localizedDescription] title:@"Error found."];
                                                           }
                                                       }];
 }
 
 - (IBAction)unsubscribe:(id)sender {
+    __weak MainViewController *safeMe = self;
+
     [self.application.pushNotifications unSubscribeDeviceUsingToken:[self.deviceToken description]
                                                         fromChannel:self.unsubscribeTextField.text
                                                          completion:^(KZResponse *r) {
                                                              if (r.error == nil) {
-                                                                 NSLog(@"response is %@", r.response);
-                                                                 NSLog(@"Did Unsubsubscribe");
+                                                                 NSString *message = [NSString stringWithFormat:@"Unsubscribed from %@", self.unsubscribeTextField.text];
+                                                                 [safeMe showAlertWith:message title:@"Unsubscribed!"];
+                                                             } else {
+                                                                 [safeMe showAlertWith:[r.error localizedDescription] title:@"Error found."];
                                                              }
     }];
 }
 
 - (IBAction)push:(id)sender {
+    
+    __weak MainViewController *safeMe = self;
     [self.application.pushNotifications pushNotification:@{@"text": self.messageTextField.text,
                                                            @"title" : @"iOS Title",
                                                            @"type" : @"raw"}
                                                InChannel:self.pushTextField.text
                                               completion:^(KZResponse *r) {
                                                   if (r.error == nil) {
-                                                      NSLog(@"response is %@", r.response);
-                                                      NSLog(@"Did push");
+                                                      NSString *message = [NSString stringWithFormat:@"Sent push action to server"];
+                                                      [safeMe showAlertWith:message title:@"Push action sent!"];
+                                                  } else {
+                                                      [safeMe showAlertWith:[r.error localizedDescription] title:@"Error found."];
                                                   }
-        
     }];
 }
 
 - (IBAction)listChannels:(id)sender {
+    __weak MainViewController *safeMe = self;
+
     [self.application.pushNotifications getSubscriptions:^(KZResponse *r) {
-        
         NSString *message = [NSString stringWithFormat:@"%@", r.response];
-        [[[UIAlertView alloc] initWithTitle:@"Subscribed channels"
-                                    message:message
-                                   delegate:nil
-                          cancelButtonTitle:@"OK"
-                          otherButtonTitles: nil] show];
+        [safeMe showAlertWith:message title:@"Subscribed channels"];
     }];
+}
+
+- (void)showAlertWith:(NSString *)message title:(NSString *)title
+{
+    [[[UIAlertView alloc] initWithTitle:title
+                                message:message
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles: nil] show];
+
 }
 
 @end
